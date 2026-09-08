6743c64c9937b050aed836d3bb4f91fb8b9f51e9
Ana-Carla-04 <153018029+Ana-Carla-04@users.noreply.github.com>
2026-09-08 13:28:10 -0300

    rodando

--- src/main/java/br/edu/ufersa/SIPA/SipaApplication.java
+++ src/main/java/br/edu/ufersa/SIPA/SipaApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SipaApplication {
 
 	public static void main(String[] args) {
