3ea3e633ad3e84ab3f68b0012f70e2b55d5063b1
João Neto <joao.neto70445@alunos.ufersa.edu.br>
2026-09-17 14:03:50 -0300

    repositorys dashboard, analise e historico

---
+++ src/main/java/br/edu/ufersa/SIPA/freatures/dashboard/DashboardRepository.java
@@ -0,0 +1,9 @@
+package br.edu.ufersa.SIPA.freatures.dashboard;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface DashboardRepository extends JpaRepository<DashboardEntity, Long> {
+
+    
+}
