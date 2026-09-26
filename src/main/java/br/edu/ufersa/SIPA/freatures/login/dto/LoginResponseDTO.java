28192894f1a05600f0dbe7776175c8769fa34a45
Hellano Lucas <herculanonft@gmail.com>
2026-09-26 01:53:14 -0300

    Creating LoginResponseDTO

---
+++ src/main/java/br/edu/ufersa/SIPA/freatures/login/dto/LoginResponseDTO.java
@@ -0,0 +1,9 @@
+package br.edu.ufersa.SIPA.freatures.login.dto;
+import br.edu.ufersa.SIPA.freatures.auth.dto.UsuarioResponseDTO;
+
+public class LoginResponseDTO (
+        String token,
+        String tipo,
+        UsuarioResponseDTO usuario
+){
+}
