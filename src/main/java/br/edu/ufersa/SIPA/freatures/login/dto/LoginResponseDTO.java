1c46934166072a686160173a0ce8e865c165b514
Hellano Lucas <herculanonft@gmail.com>
2026-09-26 02:16:13 -0300

    Implementando LoginResponseDTO

--- src/main/java/br/edu/ufersa/SIPA/freatures/login/dto/LoginResponseDTO.java
+++ src/main/java/br/edu/ufersa/SIPA/freatures/login/dto/LoginResponseDTO.java
@@ -1,9 +1,21 @@
 package br.edu.ufersa.SIPA.freatures.login.dto;
 import br.edu.ufersa.SIPA.freatures.auth.dto.UsuarioResponseDTO;
 
-public class LoginResponseDTO (
-        String token,
-        String tipo,
-        UsuarioResponseDTO usuario
-){
+public class LoginResponseDTO {
+    private String token;
+    private String tipo;
+    private UsuarioResponseDTO usuario;
+
+
+
+    public LoginResponseDTO(){}
+
+
+    public LoginResponseDTO(String token, String tipo, UsuarioResponseDTO usuario){
+        this.token = token;
+        this.tipo = tipo;
+        this.usuario = usuario;
+    }
+
+
 }
